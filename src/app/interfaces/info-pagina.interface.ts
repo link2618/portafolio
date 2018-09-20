384f167f2ca13411ab079bb5dddab70329dcd0f2
Hugo Carvajalino <link_2618@hotmail.com>
2018-09-19 23:15:43 -0500

    Fin seccion 4

---
+++ src/app/interfaces/info-pagina.interface.ts
@@ -0,0 +1,11 @@
+
+export interface InfoPagina {
+  titulo?: string;
+  email?: string;
+  nombre?: string;
+  facebook?: string;
+  twitter?: string;
+  instagram?: string;
+  tublr?: string;
+  equipo_trabajo?: any[];
+}
